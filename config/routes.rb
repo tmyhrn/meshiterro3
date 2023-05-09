2ec884f150acec83d7cf0d849ded4773c94a49c0
EC2 Default User <ec2-user@ip-172-31-21-82.ap-northeast-1.compute.internal>
2023-05-09 05:32:50 +0000

    [finish]application2-meshiterro

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :users, only: [:show, :edit, :update]
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  
+  devise_for :users
+  
+  root to: "homes#top"
+  
+  get 'homes/about' => 'homes#about', as: 'about'
+end
